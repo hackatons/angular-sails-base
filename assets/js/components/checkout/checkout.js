d1d9255f4c1dfac45283741b5915d1fe04f27b67
jevgenir <zeka.rum@gmail.com>
2016-09-11 09:58:12 +0300

    sell a car lol

--- assets/js/components/checkout/checkout.js
+++ assets/js/components/checkout/checkout.js
@@ -1,45 +1,44 @@
-
 angular.module('application').component('checkout', {
-    templateUrl: '/js/components/checkout/checkout.html',
-    controller: function ($scope, $element, $attrs) {
+  templateUrl: '/js/components/checkout/checkout.html',
+  controller: function ($scope, $element, $attrs) {
 
-        $scope.changeCheckoutTab = function() {
-            $scope.leaseTab = !$scope.leaseTab;
-        };
+    $scope.changeCheckoutTab = function () {
+      $scope.leaseTab = !$scope.leaseTab;
+    };
 
+    /*
+     * jQuery used because was most familiar
+     * */
+    var originalHeight = 0;
 
-
-        /*
-        * jQuery used because was most familiar
-        * */
-        var originalHeight = 0;
-        function fixDiv() {
-            var $cache = $('#scroller');
-            var $width = $('#scroller').parent().parent().width();
-            if ($(window).scrollTop() > $cache.offset().top) {
-                $cache.css({
-                    'position': 'fixed',
-                    'top': '0',
-                    'width': $width
-                });
-            }
-            if ($(window).scrollTop() < originalHeight) {
-                $('#scroller').css('position', 'relative');
-                $('#scroller').removeClass('top');
-            }
-        }
-        $(window).scroll(fixDiv);
-        fixDiv();
-
-        $(document).ready(function () {
-
-            window.setTimeout(function(){
-                getCheckoutCOntainerHeight()
-            },500);
+    function fixDiv() {
+      var $cache = $('#scroller');
+      var $width = $('#scroller').parent().parent().width();
+      if ($cache && $cache.offset() && $(window).scrollTop() > $cache.offset().top) {
+        $cache.css({
+          'position': 'fixed',
+          'top': '0',
+          'width': $width
         });
-        function getCheckoutCOntainerHeight(){
-            originalHeight = $("#scroller").offset().top;
-
-        }
+      }
+      if ($(window).scrollTop() < originalHeight) {
+        $('#scroller').css('position', 'relative');
+        $('#scroller').removeClass('top');
+      }
     }
+
+    $(window).scroll(fixDiv);
+    fixDiv();
+
+    $(document).ready(function () {
+
+      window.setTimeout(function () {
+        getCheckoutCOntainerHeight()
+      }, 500);
+    });
+    function getCheckoutCOntainerHeight() {
+      originalHeight = $("#scroller").offset().top;
+
+    }
+  }
 });
--- assets/js/components/sk-navbar/sk-navbar.js
+++ assets/js/components/sk-navbar/sk-navbar.js
@@ -3,5 +3,36 @@
  */
 
 angular.module('application').component('skNavbar', {
-  templateUrl: '/js/components/sk-navbar/sk-navbar.html'
+  templateUrl: '/js/components/sk-navbar/sk-navbar.html',
+  controller: function ($scope) {
+    $scope.sellCar = function () {
+      swal({
+          title: "Mark",
+          text: "Write the mark of the car you want to sell:",
+          type: "input",
+          showCancelButton: true,
+          closeOnConfirm: false,
+          animation: "slide-from-top",
+          inputPlaceholder: "Write something"
+        },
+        function (inputValue) {
+          if (inputValue === false) return false;
+          if (inputValue === "") {
+            swal.showInputError("You need to write something!");
+            return false
+          }
+          swal({
+            title: "An input!",
+            text: "Write a price",
+            type: "input",
+            showCancelButton: true,
+            closeOnConfirm: false,
+            animation: "slide-from-top",
+            inputPlaceholder: "Write something"
+          }, function (inputValue1) {
+            swal("Nice!", "You just sold: " + inputValue + " for " + inputValue1 + " EUR", "success");
+          });
+        });
+    }
+  }
 });
