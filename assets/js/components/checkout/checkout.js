12803562ee0ef2ee1bdd644e02e6e5fe17b75318
jevgenir <zeka.rum@gmail.com>
2016-09-11 10:43:28 +0300

    checkout

--- assets/js/components/checkout/checkout.js
+++ assets/js/components/checkout/checkout.js
@@ -1,93 +1,98 @@
 angular.module('application').controller('checkoutModalController', function ($uibModalInstance, notificationService, carData) {
-    var $ctrl = this;
-    $ctrl.header = 'One More Step';
-    $ctrl.user = {
-        carBrand: carData.brand,
-        carPrice: carData.price,
-        carModel: carData.model
-    };
+  var $ctrl = this;
+  $ctrl.header = 'One More Step';
+  $ctrl.user = {
+    carBrand: carData.brand,
+    carPrice: carData.price,
+    carModel: carData.model
+  };
 
-    $ctrl.cancel = function () {
-        $uibModalInstance.dismiss('cancel');
-    };
+  $ctrl.cancel = function () {
+    $uibModalInstance.dismiss('cancel');
+  };
 
-    $ctrl.submit = function(){
-        var userModel = $ctrl.user;
-        var response = notificationService.getNotificationOrder(userModel.email, userModel.carBrand, userModel.carPrice, userModel.carModel);
-        $uibModalInstance.close(reponse);
-    }
-    <!--api/notification/order?recipient=ing.edwardyrc@gmail.com&brand=mazda&price=2700&modelname=sorento-->
+  $ctrl.submit = function () {
+    var userModel = $ctrl.user;
+    var response = notificationService.getNotificationOrder(userModel.email, userModel.carBrand, userModel.carPrice, userModel.carModel);
+    $uibModalInstance.close(reponse);
+  };
+  <!--api/notification/order?recipient=ing.edwardyrc@gmail.com&brand=mazda&price=2700&modelname=sorento-->
 });
 
 angular.module('application').component('checkout', {
-    templateUrl: '/js/components/checkout/checkout.html',
-    controller: function ($scope, $element, $attrs, $uibModal) {
+  templateUrl: '/js/components/checkout/checkout.html',
+  controller: function ($scope, $element, $attrs, $uibModal) {
 
-        $scope.changeCheckoutTab = function() {
-            $scope.leaseTab = !$scope.leaseTab;
-        };
+    $scope.changeCheckoutTab = function () {
+      $scope.leaseTab = !$scope.leaseTab;
+    };
 
-        var carData = {
-            brand: 'mazda',
-            price: 2700,
-            model: 'sorento'
-        };
+    var carData = {
+      brand: this.car.brand,
+      price: this.car.price,
+      model: this.car.model
+    };
 
-        $scope.openCheckoutModal = function(){
-            var modalInstance = $uibModal.open({
-                animation: true,
-                ariaLabelledBy: 'modal-title',
-                ariaDescribedBy: 'modal-body',
-                templateUrl: '/js/components/checkout/modal/checkoutModal.html',
-                controller: 'checkoutModalController',
-                controllerAs: '$ctrl',
-            //    scope: $scope // <-- I added thise,
-                resolve: {
-                    carData: function () {
-                        return carData;
-                    }
-                }
-            });
-
-            modalInstance.result.then(function (response) {
-                $scope.successMessage = response;
-            }, function () {
-
-            });
-
+    $scope.openCheckoutModal = function () {
+      var modalInstance = $uibModal.open({
+        animation: true,
+        ariaLabelledBy: 'modal-title',
+        ariaDescribedBy: 'modal-body',
+        templateUrl: '/js/components/checkout/modal/checkoutModal.html',
+        controller: 'checkoutModalController',
+        controllerAs: '$ctrl',
+        //    scope: $scope // <-- I added thise,
+        resolve: {
+          carData: function () {
+            return carData;
+          }
         }
+      });
 
-        /*
-        * jQuery used because was most familiar
-        * */
-        var originalHeight = 0;
-        function fixDiv() {
-            var $cache = $('#scroller');
-            var $width = $('#scroller').parent().parent().width();
-            if ($(window).scrollTop() > $cache.offset().top) {
-                $cache.css({
-                    'position': 'fixed',
-                    'top': '0',
-                    'width': $width
-                });
-            }
-            if ($(window).scrollTop() < originalHeight) {
-                $('#scroller').css('position', 'relative');
-                $('#scroller').removeClass('top');
-            }
-        }
-        $(window).scroll(fixDiv);
-        fixDiv();
+      modalInstance.result.then(function (response) {
+        $scope.successMessage = response;
+      }, function () {
 
-        $(document).ready(function () {
+      });
 
-            window.setTimeout(function(){
-                getCheckoutCOntainerHeight()
-            },500);
+    };
+
+    /*
+     * jQuery used because was most familiar
+     * */
+    var originalHeight = 0;
+
+    function fixDiv() {
+      var $cache = $('#scroller');
+      var $width = $('#scroller').parent().parent().width();
+      if ($cache && $cache.offset() && $(window).scrollTop() > $cache.offset().top) {
+        $cache.css({
+          'position': 'fixed',
+          'top': '0',
+          'width': $width
         });
-        function getCheckoutCOntainerHeight(){
-            originalHeight = $("#scroller").offset().top;
-
-        }
+      }
+      if ($(window).scrollTop() < originalHeight) {
+        $('#scroller').css('position', 'relative');
+        $('#scroller').removeClass('top');
+      }
     }
+
+    $(window).scroll(fixDiv);
+    fixDiv();
+
+    $(document).ready(function () {
+
+      window.setTimeout(function () {
+        getCheckoutCOntainerHeight()
+      }, 1500);
+    });
+    function getCheckoutCOntainerHeight() {
+      originalHeight = $("#scroller").offset().top;
+
+    }
+  },
+  bindings: {
+    car: '<'
+  }
 });
