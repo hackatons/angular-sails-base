b80fea5dfb4136d9504dde0d9406c9835c452d0e
mihhail <Mihhail.Marenkov@nortal.com>
2016-09-11 12:32:58 +0300

    sell car modal problem

--- assets/js/components/sk-navbar/sk-navbar.js
+++ assets/js/components/sk-navbar/sk-navbar.js
@@ -4,9 +4,35 @@
 
 angular.module('application').component('skNavbar', {
   templateUrl: '/js/components/sk-navbar/sk-navbar.html',
-  controller: function ($scope) {
-    $scope.sellCar = function () {
+  controller: function ($scope, $uibModal) {
 
+    $scope.sellCar = function () {
+      console.log("TTEEEEE");
+
+      var modalInstance = $uibModal.open({
+        animation: true,
+        ariaLabelledBy: 'modal-title',
+        ariaDescribedBy: 'modal-body',
+        templateUrl: '/js/components/sk-navbar/sellCarModal.html'
+        //    scope: $scope // <-- I added thise,
+       /* resolve: {
+          carData: function () {
+            return carData;
+          }
+        }*/
+      });
+
+      modalInstance.result.then(function (response) {
+        $scope.successMessage = response;
+      }, function () {
+
+      });
+    };
+
+    this.submit = function(){
+      //var userModel = $ctrl.user;
+      console.log("HELLOOOOO");
     }
+
   }
 });
